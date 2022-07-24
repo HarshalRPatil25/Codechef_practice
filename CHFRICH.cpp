3f42aa5a691e2643c3e8ca6caa19b43a534369f1
HarshalRPatil25 <107293253+HarshalRPatil25@users.noreply.github.com>
2022-07-24 14:09:17 +0530

    Add files via upload

---
+++ CHFRICH.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a ,b,x;
+	    cin>>a>>b>>x;
+	    int c = (b-a)/x;
+	    std::cout << c<< std::endl;
+	    
+	}
+	return 0;
+}
